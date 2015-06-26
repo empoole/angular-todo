9e494e98dcae4d6188203a62177925a1c11ccf0f
Eric <empoole114@gmail.com>
2015-06-26 15:49:05 -0400

    Checked items turn green

--- js/functions.js
+++ js/functions.js
@@ -4,3 +4,10 @@
 function generateId() {
 	return Math.floor(Math.random() * 10000000000000001).toString();
 }
+
+//Clear the task name and task description input fields
+function clearInputs() {
+	$("#add-task-name").val("");
+	$("#add-task-desc").val("");
+}
+
--- js/mainController.js
+++ js/mainController.js
@@ -1,4 +1,5 @@
 app.controller('MainController', ['$scope', function($scope) {
+	//Array to hold task objects
 	$scope.tasks = [
 		{
 			name : "Write app",
@@ -16,27 +17,58 @@ app.controller('MainController', ['$scope', function($scope) {
 	
 	//Add a new task
 	$scope.addTask = function() {
-		var taskName = $("#add-task-name").val(),
-			taskDesc = $("#add-task-desc").val();
+		//Task name and description values from form inputs
+			var taskName = $("#add-task-name").val(),
+				taskDesc = $("#add-task-desc").val();
 
-		var newTask = {
-			name : taskName,
-			desc : taskDesc,
-			complete : false,
-			id : generateId()
+		//Only add new task if the name field is filled
+		if (taskName.length > 0) {
+			
+			//Create new task
+			var newTask = {
+				name : taskName,
+				desc : taskDesc,
+				complete : false,
+				id : generateId()
+			}
+
+			//Clear the form inputs
+			clearInputs();
+			
+			//Add new task to $scope.tasks array
+			$scope.tasks.push(newTask);
 		}
-
-		$scope.tasks.push(newTask);
 	}
+
 
 	//Remove the given task
 	$scope.removeTask = function(task) {
+		
 		//Loop through the $scope.tasks array
 		for(var i = 0; i < $scope.tasks.length; i++) {
+			
 			//Find the task to remove by its id and splice it from the array
 			if ($scope.tasks[i].id === task.id) {
 				$scope.tasks.splice(i, 1);
 			}
+		}
+	}
+
+	//Mark a task as complete
+	$scope.completeTask = function(task) {
+		var $taskId = $("#" + task.id),
+			$removeBtn = $("#" + task.id + " .btn");
+
+		if (task.complete) {
+			task.complete = false;
+			$taskId.removeClass("bg-green");
+			$removeBtn.removeClass("btn-success");
+			$removeBtn.addClass("btn-danger");
+		} else {
+			task.complete = true;
+			$taskId.addClass("bg-green");
+			$removeBtn.removeClass("btn-danger");
+			$removeBtn.addClass("btn-success");
 		}
 	}
 }]);
