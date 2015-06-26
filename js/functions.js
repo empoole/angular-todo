930c66cea65ec7164e8e595ab60d6fb950b6b9fb
Eric <empoole114@gmail.com>
2015-06-26 14:06:27 -0400

    Add and remove functions

---
+++ js/functions.js
@@ -0,0 +1,6 @@
+//Utility functions
+
+//Generate a "unique" id for elements
+function generateId() {
+	return Math.floor(Math.random() * 10000000000000001).toString();
+}
--- js/mainController.js
+++ js/mainController.js
@@ -4,16 +4,39 @@ app.controller('MainController', ['$scope', function($scope) {
 			name : "Write app",
 			desc : "Write your angular todo app",
 			complete : false,
-			id : "task1"
+			id : generateId()
 		},
 		{
 			name : "Style app",
 			desc : "Style the app with Bootstrap",
 			complete : false,
-			id : "task2"
+			id : generateId()
 		}
 	]
+	
+	//Add a new task
 	$scope.addTask = function() {
-		
+		var taskName = $("#add-task-name").val(),
+			taskDesc = $("#add-task-desc").val();
+
+		var newTask = {
+			name : taskName,
+			desc : taskDesc,
+			complete : false,
+			id : generateId()
+		}
+
+		$scope.tasks.push(newTask);
+	}
+
+	//Remove the given task
+	$scope.removeTask = function(task) {
+		//Loop through the $scope.tasks array
+		for(var i = 0; i < $scope.tasks.length; i++) {
+			//Find the task to remove by its id and splice it from the array
+			if ($scope.tasks[i].id === task.id) {
+				$scope.tasks.splice(i, 1);
+			}
+		}
 	}
 }]);
